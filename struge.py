439ed720a013c3ed58ba4d2ce110eb90e2117b4d
Steffen Köhler <steffen@koehlers.de>
2025-07-20 16:36:01 +0200

    starts to work

--- struge.py
+++ struge.py
@@ -1,5 +1,9 @@
+from typing import Optional
+
 import argparse
 from copy import copy
+import json
+from jinja2 import Environment
 import oyaml
 
 
@@ -7,23 +11,80 @@ class Node(dict):
     """dict extension class to store one element of the tree,
     recursive load and print
     """
-
+    object_count = 0
+    component_count= 0
+    layout_count = 0
     def __init__(
         self,
-        name: str,
+        parent,
+        struge_name: str,
         own_node,
         model: dict,
         components: dict,
-        layout: dict,
+        layouts: dict,
         is_component: bool,
     ):
-        self.name = name
-        for key, value in own_node.items():
-            if key == "inner":
-                self["inner"] = self.eval_inner(value, model, components, layout, True)
-            else:
-                self[key] = value
+        self.struge_name = struge_name
+        self.parent = parent
+        if "name" not in own_node:
+            Node.object_count += 1
+            self.name = f"object_{Node.object_count}"
+        else:
+            self.name= own_node["name"]
+            del own_node["name"]
+        self.content = ""
+        self.inner_content = ""
+        self.layout= ""
+        self.inner= None
+        if isinstance(own_node, str):
+            self.content = own_node
+        else:
+            for key, value in own_node.items():
+                if key == "inner":
+                    self.inner = self.eval_inner(value, model, components, layouts, True)
+                elif key == "layout":
+                    if value not in layouts:
+                        print(
+                            f"FATAL: Layout '{value}' not found in layout definitions - Aborting.."
+                        )
+                        raise ValueError(
+                            f"Layout '{value}' not found in layout definitions - Aborting.."
+                        )
+                    self.layout = layouts[value]
+                else:
+                    if key.startswith("~"):
+                        key = key[1:]  # remove the write protection
+                    self[key] = value
+            # now we inerit the parent content
+            if self.parent:
+                for key, value in parent.items():
+                    if "~"+key in own_node: # write protected key
+                        continue
+                    self[key] = value
+        #self.get_content()
 
+    def get_content(self):
+        if self.inner:
+            if isinstance(self.inner, list):
+                for sub_node in self.inner:
+                    if isinstance(sub_node, Node):
+                        self.inner_content += sub_node.get_content()
+                    else:
+                        self.inner_content += str(sub_node)
+            else:
+                    if isinstance(self.inner, Node):
+                        self.inner_content += self.inner.get_content()
+                    else:
+                        self.inner_content += str(self.inner)
+        if self.layout:
+            env = Environment()
+            t = env.from_string(self.layout)
+            self.content = t.render(item=self, inner=self.inner_content)
+            return self.content
+        else:
+            self.content = self.inner_content
+            return self.content
+    
     def eval_inner(
         self, own_node, model: dict, components: dict, layout: dict, is_component: bool
     ):
@@ -38,36 +99,44 @@ class Node(dict):
             return result
 
     def eval_single_node(
-        self, own_node, model: dict, components: dict, layout: dict, is_component: bool
+        self, node_name, model: dict, components: dict, layout: dict, is_component: bool
     ):
 
-        if own_node in model:
+        if node_name in model:
             return Node(
-                name=own_node,
-                own_node=model.get(own_node),
+                self,
+                struge_name=node_name,
+                own_node=model.get(node_name),
                 model=model,
                 components=components,
-                layout=layout,
+                layouts=layout,
                 is_component=False,
             )
-        elif own_node in components:
+        elif node_name in components:
+            Node.component_count += 1
             return Node(
-                components.get(own_node),
+                self,
+                struge_name=f"component_{Node.component_count}",
+                own_node=components.get(node_name),
                 model=model,
                 components=components,
-                layout=layout,
+                layouts=layout,
                 is_component=True,
             )
-        elif own_node in layout:
+        elif node_name in layout:
+            Node.layout_count += 1
+            # if the node is a layout, we create a new node with the name
             return Node(
-                layout.get(own_node),
+                self,
+                struge_name=f"layout_{Node.layout_count}",
+                own_node=layout.get(node_name),
                 model=model,
                 components=components,
-                layout=layout,
+                layouts=layout,
                 is_component=True,
             )
         else:
-            return own_node
+            return node_name
 
     def create_copy(
         self, own_node, model: dict, components: dict, layout: dict, is_component: bool
@@ -100,7 +169,7 @@ class Layout:
                 self.layout = oyaml.load(fin, Loader=oyaml.Loader)
         except:
             self.layout = {}
-        self.structure = None  # stores the final tree
+        self.structure: Optional[Node] = None  # stores the final tree
 
     def _parse(self):
         if "main" not in self.main:
@@ -108,6 +177,7 @@ class Layout:
                 "FATAL: Main data does not contain the 'main' root element - Aborting.."
             )
         self.structure = Node(
+            None,
             "main",
             self.main.get("main"),
             self.main,
@@ -137,4 +207,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
     layout = Layout(args.main, args.components, args.layout)
     layout._parse()
-    print(layout)
+    print(json.dumps(layout.structure,sort_keys=True, indent=4))
+    print(layout.structure.get_content())
